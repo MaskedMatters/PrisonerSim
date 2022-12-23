ab70b320b5b1f24c12575432bb9a78bb6b506133
MaskedMatters <62668426+MaskedMatters@users.noreply.github.com>
2022-12-23 10:16:25 -0500

    Fixed Percent (Float to Int)
    For some reason the float kept being "NaN" (Not a Number) or "Infinity" which confused me a lot but now it's fixed.

--- Percentage.cs
+++ Percentage.cs
@@ -3,12 +3,23 @@ namespace PrisonerSim
     public static class ConsoleUtility
     {
         public static void WritePercentage(float percent)
-        {   
+        {
+            int percentInt;
+
+            if (float.IsNaN(percent))
+                percentInt = 0;
+            else if (float.IsInfinity(percent))
+                percentInt = 0;
+            else if (percent < 0)
+                percentInt = 0;
+            else
+                percentInt = Convert.ToInt32(percent);
+
             Console.CursorVisible = false; // Set cursor visibility to false
 
             Console.SetCursorPosition(0, 3); // Start cursor at start of line
 
-            Console.Write(percent + "%"); // Write the percentage
+            Console.Write(percentInt + "%"); // Write the percentage
         }
     }
 }
